77db0c5f02c906a551818fec1471f064341a4a86
Kris Sankaran <sankaran.kris@gmail.com>
2017-04-08 20:11:35 -0700

    Canvas approach, to speed things up

--- 20170412/slides/phylo_ix.js
+++ 20170412/slides/phylo_ix.js
@@ -1,7 +1,31 @@
+function draw_phylo(context) {
+  detached_container = document.createElement("custom");
 
-function draw_phylo_ix() {
-  d3.select("#vis svg")
-    .selectAll("circle")
+	//Clear canvas
+	context.fillStyle = "#fff";
+	context.rect(0,0,canvas.attr("width"),canvas.attr("height"));
+	context.fill();
+
+  phylo_binding.each(function(d) {
+    var sample = d3.select(this);
+    context.fillStyle = sample.attr("fill");
+    context.beginPath();
+    context.arc(
+      sample.attr("cx"),
+      sample.attr("cy"),
+      sample.attr("r"),
+      0,
+      2 * Math.PI,
+      true
+    );
+    context.fill();
+    context.closePath();
+  });
+
+}
+
+function counts_phylo_ix() {
+  phylo_binding = phylo_container.selectAll("circle")
     .data(combined, function(d) { return d.Meas_ID + d.rsv; })
     .enter()
     .append("circle")
@@ -12,4 +36,15 @@ function draw_phylo_ix() {
       "r": 2,
       "opacity": 0.5
     });
+}
+
+function binarize_phylo_ix() {
+  phylo_container.selectAll("circle")
+    .transition()
+    .duration(5000)
+    .attrs({
+      "cy": function(d) { return binarize_scale(d.jittered_binarized); }
+    });
+
+  console.log("binarized")
 }
--- 20170412/slides/setup.js
+++ 20170412/slides/setup.js
@@ -1,9 +1,1 @@
 
-function setup_page() {
-  d3.select("#vis")
-    .append("svg")
-    .attrs({
-      "width": width,
-      "height": height
-    });
-}
