db491c47dfec1969ace85f2937cacae85479e858
Kris Sankaran <sankaran.kris@gmail.com>
2017-04-09 15:05:50 -0700

    Overlay path for phylo_ix

--- 20170412/slides/models.js
+++ 20170412/slides/models.js
@@ -22,3 +22,31 @@ function taxa_partial_dependence() {
       "opacity": 1
     });
 }
+
+function phylo_ix_partial_dependence() {
+  d3.selectAll(".partial_dependence")
+    .remove();
+
+  var partial_dependence_line = d3.line()
+      .x(function(d) {
+        console.log(d)
+        return scales.phylo_ix(d.phylo_ix);
+      })
+      .y(function(d) { return scales.subject(d.subject) + scales.counts(d.f_bar); });
+
+  d3.select("#vis svg")
+    .selectAll(".partial_dependence")
+    .data(f_combined["phylo_ix"])
+    .enter()
+    .append("path")
+    .attrs({
+      "opacity": 0,
+      "class": "partial_dependence",
+      "d": partial_dependence_line
+    });
+
+  d3.selectAll(".partial_dependence")
+    .transition()
+    .duration(1000)
+    .attr("opacity", 1);
+}
