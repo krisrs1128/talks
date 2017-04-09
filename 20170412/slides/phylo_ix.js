ee7c87e75bb5297380b3a4712df4c0f99d7f1b4d
Kris Sankaran <sankaran.kris@gmail.com>
2017-04-08 18:51:03 -0700

    just reproduce the ggplot

---
+++ 20170412/slides/phylo_ix.js
@@ -0,0 +1,15 @@
+
+function draw_phylo_ix() {
+  d3.select("#vis svg")
+    .selectAll("circle")
+    .data(combined, function(d) { return d.Meas_ID + d.rsv; })
+    .enter()
+    .append("circle")
+    .attrs({
+      "cx": function(d) { return phylo_ix_scale(d.phylo_ix); },
+      "cy": function(d) { return counts_scale(d.jittered_count); },
+      "fill": function(d) { return taxa_cols(d.order_top); },
+      "r": 2,
+      "opacity": 0.5
+    });
+}
