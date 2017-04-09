b1a5fe1e6991d13d119e3df4fd2b58b5a393d1f4
Kris Sankaran <sankaran.kris@gmail.com>
2017-04-09 11:39:33 -0700

    Add binarization function

--- 20170412/slides/phylo_ix.js
+++ 20170412/slides/phylo_ix.js
@@ -10,6 +10,17 @@ function draw_phylo_ix() {
       "cy": function(d) { return counts_scale(d.jittered_count); },
       "fill": function(d) { return taxa_cols(d.order_top); },
       "r": 2,
-      "opacity": 0.5
+      "opacity": 0.8
     });
+}
+
+function binarize_phylo_ix() {
+  d3.select("#vis svg")
+    .selectAll("circle")
+    .transition()
+    .duration(3000)
+    .attrs({
+      "cy": function(d) { return binarized_scale(d.jittered_binarized); }
+    });
+
 }
