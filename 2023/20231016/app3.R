e42bef6bbc80ed22ae6f4b2186a26fc216281640
Kris Sankaran <sankaran.kris@gmail.com>
2023-10-15 18:58:06 -0500

    another example

--- 2023/20231016/app3.R
+++ 2023/20231016/app3.R
@@ -4,31 +4,32 @@ library(shiny)
 theme_set(theme_classic())
 
 ui <- fluidPage(
-  sliderInput("N", "Sample Size", 5, 500, 10),
+  sliderInput("N", "Sample Size", 5, 500, 150),
   sliderInput("beta_1", "beta_1", -2, 2, 1, step = 0.1),
   sliderInput("beta_2", "beta_2", -2, 2, -1, step = 0.1),
-  sliderInput("rho", "rho", 0, 1, 0),
+  sliderInput("n_irrelevant", "irrelevant_features", 0, 100, 15),
   sliderInput("sigma", "sigma", 0.001, 10, 1),
   fluidRow(
     column(plotOutput("beta_hats"), width = 4),
-    column(rglwidgetOutput("surface"), width = 4)
+    column(plotOutput("surface"), width = 4)
   )
 )
 
 server <- function(input, output) {
   generate_data <- reactive({
     X <- matrix(rnorm(input$N * 2), input$N, 2)
-    X <- X %*% matrix(c(1, sqrt(input$rho), sqrt(input$rho), 1), nrow = 2)
+    X <- cbind(X, matrix(rnorm(input$N * input$n_irrelevant), nrow = input$N, ncol = input$n_irrelevant))
     
-    beta <- c(input$beta_1, input$beta_2)
+    beta <- c(input$beta_1, input$beta_2, rep(0, input$n_irrelevant))
     mu <- X %*% beta
     y <- mu + rnorm(input$N, 0, input$sigma)
     data.frame(X = X, y = y, y_mean = mu)
   })
   
   beta_hat <- reactive({
-    X <- generate_data()
-    coef(lm(y ~ X.1 + X.2, data = X))
+    X <- generate_data() |>
+      select(y, starts_with("X"))
+    coef(lm(y ~ ., data = X))
   })
   
   output$beta_hats <- renderPlot({
@@ -36,7 +37,10 @@ server <- function(input, output) {
     for (i in seq_len(200)) {
       X <- generate_data()
       X$y <- X$y_mean + rnorm(input$N, 0, input$sigma)
-      beta_hats[[i]] <- coef(lm(y ~ X.1 + X.2, data = X))
+      beta_hats[[i]] <- X |>
+        select(y, starts_with("X")) |>
+        lm(y ~ ., data = _) |>
+        coef()
     }
     
     bind_rows(beta_hats) |>
@@ -57,14 +61,15 @@ server <- function(input, output) {
       )
   })
   
-  output$surface <- renderRglwidget({
-    beta_ <- beta_hat()
-    
-    open3d(useNULL=T)
-    plot3d(generate_data())
-    planes3d(input$beta_1, input$beta_2, -1, col = "red", alpha = 0.3)
-    planes3d(beta_[2], beta_[3], -1, col = "purple", alpha = 0.3)
-    rglwidget()
+  output$surface <- renderPlot({
+    generate_data() |>
+      select(starts_with("X"), y, y_mean) |>
+      pivot_longer(starts_with("X"), names_to = "dimension") |>
+      separate("dimension", c("X", "dimension"), convert = TRUE) |>
+      filter(dimension < 16) |>
+      ggplot(aes(value, y)) +
+      geom_point() +
+      facet_wrap(~ dimension)
   })
 }
 
