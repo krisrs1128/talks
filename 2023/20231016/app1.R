af598290a3ea78cb15912869c2b26b8bee4c8db9
Kris Sankaran <sankaran.kris@gmail.com>
2023-10-15 17:52:41 -0500

    add example app

--- 2023/20231016/app1.R
+++ 2023/20231016/app1.R
@@ -1,46 +1,73 @@
 library(tidyverse)
 library(rgl)
-
-N <- 10
-sigma <- 1
-X <- matrix(rnorm(N * 2), N, 2)
-beta <- c(1, 3)
-y <- X %*% beta + rnorm(N, 0, sigma)
-
-data <- data.frame(X = X, y = y)
-n_grid <- 25
-grid <- expand.grid(
-  seq(0, 2, length.out = n_grid),
-  seq(2, 4, length.out = n_grid)
+library(shiny)
+ui <- fluidPage(
+  sliderInput("N", "Sample Size", 5, 500, 10),
+  sliderInput("beta_1", "beta_1", -2, 2, 1, step = 0.1),
+  sliderInput("beta_2", "beta_2", -2, 2, -1, step = 0.1),
+  sliderInput("sigma", "sigma", 0.001, 10, 1),
+  fluidRow(
+    column(plotOutput("rss"), width = 4),
+    column(rglwidgetOutput("surface"), width = 4)
+  )
 )
 
-errors <- list()
-for (i in seq_len(nrow(grid))) {
-  y_hat <- X %*% t(grid[i, ])
-  errors[[i]] <- data.frame(
-    beta_1 = grid[i, 1],
-    beta_2 = grid[i, 2],
-    RSS = sum((y - y_hat) ^ 2)
-  )
+server <- function(input, output) {
+  generate_data <- reactive({
+    X <- matrix(rnorm(input$N * 2), input$N, 2)
+    beta <- c(input$beta_1, input$beta_2)
+    y <- X %*% beta + rnorm(input$N, 0, input$sigma)
+    data.frame(X = X, y = y)
+  })
+  
+  beta_hat <- reactive({
+    X <- generate_data()
+    coef(lm(y ~ ., data = X))
+  })
+
+  output$rss <- renderPlot({
+    grid <- expand.grid(
+      seq(input$beta_1 - 1, input$beta_1 + 2, length.out = n_grid),
+      seq(input$beta_2 - 2, input$beta_2 + 2, length.out = n_grid)
+    )
+
+    data <- generate_data()
+    X <- as.matrix(data[, 1:2])
+    y <- data$y
+    
+    errors <- list()
+    for (i in seq_len(nrow(grid))) {
+      y_hat <- X %*% t(grid[i, ])
+      errors[[i]] <- data.frame(
+        beta_1 = grid[i, 1],
+        beta_2 = grid[i, 2],
+        RSS = sum((y - y_hat) ^ 2)
+      )
+    }
+
+    beta_ <- beta_hat()
+    errors <- bind_rows(errors)
+    ggplot(errors, aes(beta_1, beta_2)) +
+      geom_tile(aes(fill = RSS, col = RSS)) +
+      stat_contour(aes(z = RSS), col = "black", bins = 10) +
+      geom_point(data = data.frame(beta_1 = input$beta_1, beta_2 = input$beta_2), size = 5, col = "red") +
+      geom_point(data = data.frame(beta_1 = beta_[2], beta_2 = beta_[3]), size = 5, col = "purple") +
+      scale_x_continuous(expand = c(0, 0)) +
+      scale_y_continuous(expand = c(0, 0)) +
+      scale_fill_distiller(direction = 1) +
+      scale_color_distiller(direction = 1) +
+      coord_fixed()
+  })
+
+  output$surface <- renderRglwidget({
+    beta_ <- beta_hat()
+    
+    open3d(useNULL=T)
+    plot3d(generate_data())
+    planes3d(input$beta_1, input$beta_2, -1, col = "red", alpha = 0.3)
+    planes3d(beta_[2], beta_[3], -1, col = "purple", alpha = 0.3)
+    rglwidget()
+  })
 }
 
-errors <- bind_rows(errors)
-fit <- lm(y ~ X.1 + X.2, data = data)
-beta_hat <- coef(fit)
-
-ggplot(errors, aes(beta_1, beta_2)) +
-  geom_tile(aes(fill = RSS, col = RSS)) +
-  stat_contour(aes(z = RSS), col = "black", bins = 10) +
-  geom_point(data = data.frame(beta_1 = beta[1], beta_2 = beta[2]), size = 5, col = "red") +
-  geom_point(data = data.frame(beta_1 = beta_hat[2], beta_2 = beta_hat[3]), size = 5, col = "purple") +
-  scale_x_continuous(expand = c(0, 0)) +
-  scale_y_continuous(expand = c(0, 0)) +
-  scale_fill_distiller(direction = 1) +
-  scale_color_distiller(direction = 1) +
-  coord_fixed()
-
-plot3d(data)
-planes3d(beta[1], beta[2], -1, col = "red", alpha = 0.3)
-planes3d(beta_hat[2], beta_hat[3], -1, col = "purple", alpha = 0.3)
-
-
+shinyApp(ui, server)
