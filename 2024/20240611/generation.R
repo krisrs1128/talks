3fa92096b2818764843c1839d49397766919dae7
Kris Sankaran <sankaran.kris@gmail.com>
2024-05-21 16:11:42 +1000

    example figure

--- 2024/20240611/generation.R
+++ 2024/20240611/generation.R
@@ -17,10 +17,10 @@ x <- matrix(nrow = 6, ncol = N)
 for (j in 1:6) {
   f <- multimedia:::spline_fun(knots = seq(0.1, 0.9, 0.2), sd = 0, D = 1)
   if (j > 3) {
-    x[j, ] <- c(f(u), f(u)) + rnorm(N, sd = 0.1)
+    x[j, ] <- c(f(u), f(u)) + rnorm(N, sd = 0.2)
   } else {
     f2 <- multimedia:::spline_fun(knots = seq(0.1, 0.9, 0.2), sd = 0, D = 1)
-    x[j, ] <- c(f(u), exp(-1) * f(u)) + rnorm(N, sd = 0.1)
+    x[j, ] <- c(f(u), exp(-1) * f(u)) + rnorm(N, sd = 0.2)
   }
 }
 
@@ -29,3 +29,4 @@ exper_ts <- SummarizedExperiment(
   colData = data.frame(group = rep(c("A", "B"), each = N / 2), time = c(u, u))
 )
 rownames(exper_ts) <- 1:6
+saveRDS(exper_ts, "exper_ts.rds")
